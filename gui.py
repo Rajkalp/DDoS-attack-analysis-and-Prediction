063f91a94b4cba69dafdc1ff88d70848f542d769
Rajkalp <minatecarry@gmail.com>
2021-04-17 10:12:58 +0530

    Predition model integrated

--- gui.py
+++ gui.py
@@ -11,6 +11,16 @@ print(emp)
 
 top.title('DDos Attack Prediction')
 
+def pred():
+    datapoint = pd.DataFrame({"src_port": [src_port.get()],
+    "dst_port":[dst_port.get()],
+    "flow_duration":[flow_d.get()],
+    "tot_fwd_pkts":[tfp.get()],"tot_bwd_pkts":[tbp.get()],"totLen_fwd_pkts": [tlfp.get()],"totLen_bwd_pkts":[tlbp.get()],
+     "fwd_pkts_per_s": [fps.get()], "bwd_pkts_per_s": [bps.get()]})
+    
+    print(emp.predict(datapoint)[0])
+    return my_string_var.set("It's a DDoS Attack") if emp.predict(datapoint)[0] == 1 else my_string_var.set("Not a DDoS Attack")
+
 mylabel = Label(top, text="Source Port:").grid(row=1, column=0)
 src_port = Entry(top,width=50)
 src_port.grid(row=1,column=1)
