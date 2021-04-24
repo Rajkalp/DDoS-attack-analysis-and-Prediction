4e0c5c6a54da0919d86786e6f678241f3d4fe35b
Rajkalp <minatecarry@gmail.com>
2021-04-24 18:43:25 +0530

    Corrections

--- model_creation.py
+++ model_creation.py
@@ -44,6 +44,13 @@ print(df.shape)
 print(X.shape)
 print(Y.shape)
 
+from sklearn.model_selection import train_test_split
+seed = 7
+test_size = 0.33
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+
+
 from sklearn.linear_model import LogisticRegression
 model=LogisticRegression()
 model.fit(X_train, y_train)
